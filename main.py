3f744319ce2bed0e0c9b0746502c6bd16eb841aa
DashaRudas <dashka.rudas@gmail.com>
2019-10-27 22:33:15 +0700

    Square

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-pass
+import turtle
+turtle.shape("turtle")
+turtle.forward (100)
+turtle.left (90)
+turtle.forward (100)
+turtle.left (90)
+turtle.forward (100)
+turtle.left (90)
+turtle.forward (100)
+turtle.left (90)
