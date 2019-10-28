ab8acbff9432e5aa1ca216a330d716720d713afe
DashaRudas <dashka.rudas@gmail.com>
2019-10-28 19:55:15 +0700

    Parts of job

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import turtle
-turtle.shape("turtle")
-turtle.forward (100)
-turtle.left (90)
-turtle.forward (100)
-turtle.left (90)
-turtle.forward (100)
-turtle.left (90)
-turtle.forward (100)
-turtle.left (90)
+# Case 1
+def square(-30,-50,40,green,0)
+# TODO (Dasha)figure house
+pass
+def circle (40, -40, 50,lightblue, 0)
+# TODO (Ira) figure snowman
+pass
+def circle (50, 50, 20, yellow, 0)
+# TODO (Dasha) figure moon
+pass
+def  (40, -40, 20,yellow, 0)
+# TODO (Ira) figure star
+pass
