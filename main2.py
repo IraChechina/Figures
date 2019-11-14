d8f2c423dbbfc1818b4b528a44454c567c283ed5
IraChechina <Irchalime@gmail.com>
2019-11-14 17:08:02 +0700

    new great file

---
+++ main2.py
@@ -0,0 +1,17 @@
+#Case 1
+#TO DO: (Dasha) square function
+pass
+#TO DO:(Dasha) triangle function
+pass
+#TO DO:(Ira) circle function
+pass
+#(Dasha): figure tree
+pass
+#(Dasha): figure house
+pass
+#(Ira): figure snowman
+pass
+#(Ira): figure present
+pass
+
+
