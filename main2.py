a9439767f487227c1f9ca6a7ab62bee4d2b31b40
IraChechina <Irchalime@gmail.com>
2019-11-14 23:03:09 +0700

    fox and present

--- main2.py
+++ main2.py
@@ -3,13 +3,13 @@
 pass
 # TO DO:(Dasha) triangle function
 pass
-# TO DO:(Ira) circle function
+# TO DO:(Ira) rhombus function
 pass
 # (Dasha): figure tree
 pass
 # (Dasha): figure house
 pass
-# (Ira): figure snowman
+# (Ira): figure fox
 pass
 # (Ira): figure present
 pass
@@ -65,3 +65,41 @@ square(0, -150, 40, 50, "brown", "brown")  # Рисуем ствол ёлки
 triangle(-105, -150, 250, "green", "green")  # Рисуем крону
 triangle(-80, -30, 200, "green", "green")
 triangle(-55, 90, 150, "green", "green")
+
+
+def rhombus(x, y, a, color, colorinside):  # переменная ромб
+    turtle.pu()
+    turtle.goto(x, y)
+    turtle.pd()
+    turtle.color(color, colorinside)
+    turtle.begin_fill()
+    turtle.fd(a)
+    turtle.rt(60)
+    turtle.fd(a)
+    turtle.rt(120)
+    turtle.fd(a)
+    turtle.rt(60)
+    turtle.fd(a)
+    turtle.rt(120)
+    turtle.end_fill()
+    turtle.pu()
+
+
+triangle(210, 0, 100, "yellow", "yellow")  # рисуем голову лисички
+square(310, 0, -100, 150, "orange", "orange")  # рисуем тело лисы
+rhombus(460, 0, -60, "red", "red")  # хвост
+triangle(460, -100, -40, "yellow", "yellow")  # рисуем лапки лисы
+triangle(310, -100, -40, "yellow", "yellow")
+triangle(260, 90, -40, "orange", "orange")  # рисуем ушко лисы
+turtle.pu()  # рисуем  глаз лисы
+turtle.goto(255, 30)
+turtle.pd()
+turtle.dot()
+turtle.pu()
+
+square(-500, -200, 150, 150, "pink", "pink")  # рисуем коробку подарка
+square(-575, -200, -150, 10, "blue", "blue")  # рисуем ленточки
+square(-650, -125, 150, 10, "blue", "blue")
+rhombus(-575, -50, 60, "blue", "blue")
+
+turtle.done()
